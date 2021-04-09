1bb03ae4c7e790a1bc28e2cfed217c4e571361f4
antonpodkur <anton.podkur@gmail.com>
2021-04-09 15:07:12 +0300

    json

--- index.js
+++ index.js
@@ -1,73 +1,12 @@
 const express = require('express');
 const multer = require('multer');
 const cors = require('cors');
+const csv = require('csvtojson')
 require('dotenv').config();
 
 const app = express();
 
 app.use(express.json())
-
-// var corsMiddleware = function(req, res, next) {
-//     res.header('Access-Control-Allow-Origin', '*');
-//     res.header('Access-Control-Allow-Methods', 'OPTIONS, GET, PUT, PATCH, POST, DELETE');
-//     res.header('Access-Control-Allow-Headers', 'Content-Type, X-Requested-With, Authorization');
-
-//     next();
-// }
-
-// app.use(corsMiddleware);
-
-// app.use(express.json());
-
-// app.use(cors());
-
-// const allowedOrigins = ["http://localhost:5002", "http://localhost:8080"];
-// const allowedOrigins = ["*"];
-
-
-// app.use(
-//     cors({
-//         origin: function (origin, callback) {
-//             // allow requests with no origin
-//             // (like mobile apps or curl requests)
-//             if (!origin) return callback(null, true);
-//             if (allowedOrigins.indexOf(origin) === -1) {
-//                 let msg =
-//                     "The CORS policy for this site does not " +
-//                     "allow access from the specified Origin.";
-//                 return callback(new Error(msg), false);
-//             }
-//             return callback(null, true);
-//         }
-//     })
-// );
-
-
-// app.use(function (req, res, next) {
-//     let origin = req.headers.origin;
-//     if (allowedOrigins.includes(origin)) {
-//         res.header("Access-Control-Allow-Origin", origin); // restrict it to the required domain
-//     }
-
-//     res.header(
-//         "Access-Control-Allow-Headers",
-//         "Origin, X-Requested-With, Content-Type, Accept"
-//     );
-//     next();
-// });
-
-// app.use((req,res, next) => {
-//     res.header("Access-Control-Allow-Origin", "*");
-//     res.header(
-//         "Access-Control-Allow-Headers",
-//         "Origin, X-Requested-With, Content-Type, Accept, Authorization"
-//     );
-//     if(req.method === 'OPTIONS') {
-//         res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
-//         res.status(200).json({});
-//     }
-//     next();
-// });
 
 const corsOptions = {
     origin: "*"
@@ -76,6 +15,7 @@ app.use(cors(corsOptions));
 
 
 const uploadFolder = "./uploads";
+let fileName = '';
 
 const storage = multer.diskStorage({
     destination(req, file, cb){
@@ -86,7 +26,7 @@ const storage = multer.diskStorage({
 
         let ogName = originalName.split('.')[0]
 
-        let someNameToSet = JSON.parse(req.body.storeName)
+        let someNameToSet = 'document'
 
         const fileExtension = (originalName.match(/\.+[\S]+$/) || [])[0];
 
@@ -97,14 +37,13 @@ const storage = multer.diskStorage({
 const upload = multer({storage});
 
 
-// app.get("/document", (req,res) => {
-//     res.send("<h2>Привет Express!</h2>");
-// });
-
-
 app.post("/document", upload.array('file',1), async function(req, res) {
     const docFileName = req.files[0].filename;
     const docLocalUrl = req.files[0].path;
+    fileName = './' + docLocalUrl;
+    console.log(fileName);
+    const jsonArr = await csv().fromFile(fileName);
+    console.log(jsonArr);
 })
 
 
